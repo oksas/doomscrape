f87ea61f7e7163ef3accae5ac758dd78b1edb68f
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-12 20:01:06 -0800

    Add tests for pageUtils.hasPosts

--- test/worker/pageUtils.spec.js
+++ test/worker/pageUtils.spec.js
@@ -3,4 +3,56 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var expect = chai.expect;
+var jsdom = require('jsdom').jsdom;
+var request = require('request-promise-native');
+var pageUtils = require('../../worker/pageUtils');
 /* eslint-enable no-unused-vars */
+
+// 30 posts
+let fullPageUrl = 'https://www.doomworld.com/vb/doom-general/42866-post-your-doom-picture-post-in-part-2-instead/400';
+
+// 0 posts
+let emptyPageUrl = 'https://www.doomworld.com/vb/doom-general/42866-post-your-doom-picture-post-in-part-2-instead/522';
+
+// 1 - 29 posts, inclusive
+let partialPageUrl = 'https://www.doomworld.com/vb/doom-general/91781-is-there-a-clean-textless-version-of-the-heretic-tome-of-power-art/';
+
+describe('pageUtils', () => {
+	describe('hasPosts', () => {
+		it('should return true if a page has at least 1 post', done => {
+			request(partialPageUrl)
+			.then(body => {
+				let document = jsdom(body);
+				let hasPosts = pageUtils.hasPosts(document);
+				expect(hasPosts).to.equal(true);
+			})
+			.then(() => {
+				done();
+			});
+		});
+
+		it('should return true if a page has 30 posts', done => {
+			request(fullPageUrl)
+			.then(body => {
+				let document = jsdom(body);
+				let hasPosts = pageUtils.hasPosts(document);
+				expect(hasPosts).to.equal(true);
+			})
+			.then(() => {
+				done();
+			});
+		});
+
+		it('should return false if a page has no posts', done => {
+			request(emptyPageUrl)
+			.then(body => {
+				let document = jsdom(body);
+				let hasPosts = pageUtils.hasPosts(document);
+				expect(hasPosts).to.equal(false);
+			})
+			.then(() => {
+				done();
+			});
+		});
+	});
+});
