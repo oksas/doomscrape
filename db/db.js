ae91d55637647a7c389c43687cb0548324144287
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-12 18:02:05 -0800

    Add config options to db connection

--- db/db.js
+++ db/db.js
@@ -1,4 +1,7 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost/doomscrape');
+var db = new Sequelize(process.env.DATABASE_URL || 'postgresql://localhost/doomscrape', {
+	dialect: 'postgres',
+	logging: false
+});
 
 module.exports = db;
