ae9abaed9745f726ca77a5b3ef96495dbd398447
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-12 19:10:48 -0800

    Add basic test for getImages

---
+++ test/worker/getImages.spec.js
@@ -0,0 +1,20 @@
+/* eslint-env node, mocha */
+/* eslint-disable no-unused-vars */
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+var getImages = require('../../worker/getImages');
+/* eslint-enable no-unused-vars */
+
+let url = 'https://www.doomworld.com/vb/doom-general/70830-post-your-doom-picture-part-2-read-the-image-posting-rules-in-the-faq/141/';
+
+describe('getImages', () => {
+	it('should find the proper number of images on a full page', done => {
+		getImages(url)
+		.then(images => {
+			console.log('sup dawg');
+			expect(images.length).to.equal(12);
+			done();
+		});
+	});
+});
---
+++ test/worker/pageUtils.spec.js
@@ -0,0 +1,6 @@
+/* eslint-env node, mocha */
+/* eslint-disable no-unused-vars */
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+/* eslint-enable no-unused-vars */
