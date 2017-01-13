9721d02f7cb93b78aa67917f7079002cf8a3a2cd
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-12 20:01:32 -0800

    Refactor core of getImages into pageUtils

--- worker/getImages.js
+++ worker/getImages.js
@@ -1,6 +1,5 @@
 var request = require('request-promise-native');
 var jsdom = require('jsdom').jsdom;
-var getImgurImages = require('./getImgurImages');
 var pageUtils = require('./pageUtils');
 
 module.exports = function getImages(url, callback) {
@@ -22,71 +21,15 @@ module.exports = function getImages(url, callback) {
 
 		let images = [];
 
-    // count is now the num of posts on the page
+		// will need to use Promise.all here to account for getting imgur images,
+		// which is another async operation
 		for (var i = 0; i < count; i++) {
-			var post = document.querySelector(`body > table:nth-of-type(${pageUtils.postBase + i + 1})`);
-
-			var postAuthor = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(1) td:nth-of-type(1) font b`).innerHTML;
-			console.log(`Post author is ${postAuthor}`);
-
-			var postDate = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(2) td:nth-of-type(1) font`).childNodes[1].nodeValue;
-			postDate = new Date(postDate);
-
-			var postPermalink = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(2) td:nth-of-type(1) font`).childNodes[4].href;
-
-			var postId = /post\/(\d+)/g.exec(postPermalink)[1];
-
-			var postContent = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(1) td:nth-of-type(2) font:nth-of-type(2)`);
-
-			var imageCount = 0;
-
-			var allPostData = {
-				author: postAuthor,
-				permalink: postPermalink,
-				date: postDate
-			};
-
-			[].forEach.call(postContent.childNodes, function(node) {
-				var postData,
-					src;
-
-				if (node.nodeName === 'IMG') {
-					src = node.src;
-					postData = Object.assign({}, allPostData, {
-						image: src,
-						id: `${postId}_${++imageCount}`
-					});
-
-					images.push(postData);
-				} else if (node.nodeName === 'A' &&
-                    node.firstElementChild &&
-                    node.firstElementChild.nodeName === 'IMG' &&
-										node.firstElementChild.src.includes('imgur.com')) {
-					postData = Object.assign({}, allPostData, {
-						postId: postId,
-						startIndex: ++imageCount
-					});
-
-					getImgurImages(node.href, postData, callback);
-				} else if (node.nodeName === 'A' &&
-                    node.firstElementChild &&
-                    node.firstElementChild.nodeName === 'IMG') {
-					src = node.firstElementChild.src;
-					postData = Object.assign({}, allPostData, {
-						image: src,
-						id: `${postId}_${++imageCount}`
-					});
-
-					images.push(postData);
-				} else if (node.nodeName === 'A' &&
-                    node.host.includes('imgur.com')) {
-					postData = Object.assign({}, allPostData, {
-						postId: postId,
-						startIndex: ++imageCount
-					});
-					getImgurImages(node.href, postData, callback);
-				}
-			});
+			let postData = pageUtils.getPostData(document, i);
+			if (postData) {
+				// use concat so that if a post has no images in it and returns [],
+				// no invalid elems are added to the images array
+				images = images.concat(postData);
+			}
 		}
 
 		return images;
--- worker/pageUtils.js
+++ worker/pageUtils.js
@@ -1,3 +1,5 @@
+// var getImgurImages = require('./getImgurImages');
+
 let pageUtils = {
 	postBase: 5,
 
@@ -16,6 +18,76 @@ let pageUtils = {
 		}
 
 		return count;
+	},
+
+	getPostData(document, i) {
+		let post = document.querySelector(`body > table:nth-of-type(${this.postBase + i + 1})`);
+
+		let postAuthor = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(1) td:nth-of-type(1) font b`).innerHTML;
+		console.log(`Post author is ${postAuthor}`);
+
+		let postDate = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(2) td:nth-of-type(1) font`).childNodes[1].nodeValue;
+		postDate = new Date(postDate);
+
+		let postPermalink = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(2) td:nth-of-type(1) font`).childNodes[4].href;
+
+		let postId = /post\/(\d+)/g.exec(postPermalink)[1];
+
+		let postContent = post.querySelector(`tr td:nth-of-type(2) table tbody tr td table tbody tr:nth-of-type(1) td:nth-of-type(2) font:nth-of-type(2)`);
+
+		let imageCount = 0;
+
+		let allPostData = {
+			author: postAuthor,
+			permalink: postPermalink,
+			date: postDate
+		};
+
+		let images = [];
+
+		[].forEach.call(postContent.childNodes, function(node) {
+			let postData,
+				src;
+
+			if (node.nodeName === 'IMG') {
+				src = node.src;
+				postData = Object.assign({}, allPostData, {
+					image: src,
+					id: `${postId}_${++imageCount}`
+				});
+
+				images.push(postData);
+			} else if (node.nodeName === 'A' &&
+									node.firstElementChild &&
+									node.firstElementChild.nodeName === 'IMG' &&
+									node.firstElementChild.src.includes('imgur.com')) {
+				postData = Object.assign({}, allPostData, {
+					postId: postId,
+					startIndex: ++imageCount
+				});
+
+				// getImgurImages(node.href, postData, callback);
+			} else if (node.nodeName === 'A' &&
+									node.firstElementChild &&
+									node.firstElementChild.nodeName === 'IMG') {
+				src = node.firstElementChild.src;
+				postData = Object.assign({}, allPostData, {
+					image: src,
+					id: `${postId}_${++imageCount}`
+				});
+
+				images.push(postData);
+			} else if (node.nodeName === 'A' &&
+									node.host.includes('imgur.com')) {
+				postData = Object.assign({}, allPostData, {
+					postId: postId,
+					startIndex: ++imageCount
+				});
+				// getImgurImages(node.href, postData, callback);
+			}
+		});
+
+		return images;
 	}
 };
 
