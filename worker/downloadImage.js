7c3e4186a239d54f9b40ccc07c8f7f74519754bc
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-13 12:28:38 -0800

    Fix formatting of downloadImage

--- worker/downloadImage.js
+++ worker/downloadImage.js
@@ -1,98 +1,90 @@
-var fs = require("fs");
-var rp = require("request-promise");
-var easyimage = require("easyimage");
-var sizeOf = require("image-size");
-var imageConfig = require("./imageConfig");
+var fs = require('fs');
+var request = require('request-promise-native');
+var easyimage = require('easyimage');
+var sizeOf = require('image-size');
+var imageConfig = require('./imageConfig');
 
 function downloadImage(imageData, callback) {
-  var ext = "";
+	let author = imageData.author.toLowerCase();
+	let id = imageData.id;
 
-  var author = imageData.author.toLowerCase();
-  var id = imageData.id;
+	let options = {
+		uri: imageData.image,
+		resolveWithFullResponse: true,
+		encoding: null
+	};
 
-  var options = {
-    uri: imageData.image,
-    resolveWithFullResponse: true,
-    encoding: null
-  };
+	request(options)
+	.then(function(response) {
+		if (response.headers['content-type'].substr(0, 5) !== 'image') {
+			return callback('Not an image');
+		}
 
-  rp(options)
-    .then(function(response) {
+		let ext = imageConfig.extMappings[response.headers['content-type']];
 
-      if (response.headers["content-type"].substr(0, 5) !== "image") {
-        return callback("Not an image");
-      }
+		let imagePath = `${imageConfig.basePath}${author}_${id}.${ext}`;
 
-      var ext = imageConfig.extMappings[response.headers["content-type"]];
+		fs.writeFile(imagePath, response.body, fileWritten);
 
-      var imagePath = `${imageConfig.basePath}${author}_${id}.${ext}`;
+		function fileWritten(err) {
+			if (err) return callback(err);
+			console.log(`Saved ${author}_${id}`);
+			sizeOf(imagePath, fileSized);
+		}
 
-      fs.writeFile(imagePath, response.body, fileWritten);
+		function fileSized(err, dimensions) {
+			if (err ||
+			dimensions.width < imageConfig.minSizes.w ||
+			dimensions.height < imageConfig.minSizes.h) {
+				fs.unlink(imagePath, fileDeleted);
+				return console.log(`Image ${id} from ${author} is too small, probably`);
+			}
 
-      function fileWritten(err) {
-        if (err) return callback(err);
-        console.log(`Saved ${author}_${id}`);
-        sizeOf(imagePath, fileSized);
-      }
+			let fullData = {
+				author: imageData.author,
+				postlink: imageData.postlink,
+				date: imageData.date,
+				filename: `${author}_${id}.${ext}`,
+				thumbname: `${author}_${id}_thumb.${ext}`,
+				filepath: imageConfig.basePath
+			};
 
-      function fileSized(err, dimensions) {
-        if (err ||
-            dimensions.width < imageConfig.minSizes.w ||
-            dimensions.height < imageConfig.minSizes.h) {
-          fs.unlink(imagePath, fileDeleted);
-          return console.log(`Image ${id} from ${author} is too small, probably`);
-        }
+			createThumbnail(fullData)
+			.then(thumbnailCreated, thumbnailError);
+		}
 
-        var fullData = {
-          author: imageData.author,
-          _id: imageData.id,
-          permalink: imageData.permalink,
-          date: imageData.date,
-          filename: `${author}_${id}.${ext}`,
-          thumbname: `${author}_${id}_thumb.${ext}`,
-          filepath: imageConfig.basePath,
-          meta: {
-            reportCount: 0
-          }
-        };
+		function fileDeleted(err) {
+			if (err) return callback(`There was an error deleting file ${imagePath}`);
+			console.log(`Successfully deleted ${imagePath}`);
+		}
 
-        createThumbnail(fullData)
-          .then(thumbnailCreated, thumbnailError);
+		function createThumbnail(imageData) {
+			return easyimage.thumbnail({
+				src: imageData.filepath + imageData.filename,
+				dst: imageData.filepath + imageData.thumbname,
+				width: imageConfig.thumbSizes.w,
+				height: imageConfig.thumbSizes.h,
+				x: 0,
+				y: 0
+			});
+		}
 
-      }
+		function thumbnailCreated() {
+			console.log(`Created thumbnail ${fullData.thumbname}`);
+			callback(null, fullData);
+		}
 
-      function fileDeleted(err) {
-        if (err) return callback(`There was an error deleting file ${imagePath}`);
-        console.log(`Successfully deleted ${imagePath}`);
-      }
-
-      function createThumbnail(imageData) {
-        return easyimage.thumbnail({
-          src: imageData.filepath + imageData.filename,
-          dst: imageData.filepath + imageData.thumbname,
-          width: imageConfig.thumbSizes.w,
-          height: imageConfig.thumbSizes.h,
-          x: 0, y: 0
-        });
-      }
-
-      function thumbnailCreated() {
-        console.log(`Created thumbnail ${fullData.thumbname}`);
-        callback(null, fullData);
-      }
-
-      function thumbnailError(err) {
-        if (err) callback(`ERROR MAKING THUMBNAIL: \n${err}`);
-      }
-
-    })
-    .catch(function(err) {
-      if (err) {
-        console.log("============");
-        console.log(`THERE WAS AN ERROR WITH \n${imageData.image} from post \n${imageData.permalink}`);
-        console.log("============");
-      }
-    });
+		function thumbnailError(err) {
+			if (err) callback(`ERROR MAKING THUMBNAIL: \n${err}`);
+		}
+	})
+	.catch(function(err) {
+		if (err) {
+			console.log('============');
+			console.log(`THERE WAS AN ERROR WITH \n${imageData.image} from post \n${imageData.postlink}`);
+			console.log('============');
+		}
+	});
 }
 
 module.exports = downloadImage;
