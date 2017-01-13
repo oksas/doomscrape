c005368d6c8b71aca95af2a2e5f89736f51f7e32
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-13 12:19:42 -0800

    Fix formatting in imageConfig.js

--- worker/imageConfig.js
+++ worker/imageConfig.js
@@ -1,19 +1,21 @@
-module.exports = {
-  extMappings: {
-    "image/jpeg": "jpg",
-    "image/png": "png",
-    "image/gif": "gif"
-  },
+let imageConfig = {
+	extMappings: {
+		'image/jpeg': 'jpg',
+		'image/png': 'png',
+		'image/gif': 'gif'
+	},
 
-  thumbSizes: {
-    w: 176,
-    h: 100
-  },
+	thumbSizes: {
+		w: 176,
+		h: 100
+	},
 
-  minSizes: {
-    w: 200,
-    h: 200
-  },
+	minSizes: {
+		w: 200,
+		h: 200
+	},
 
-  basePath: "public/images/"
+	basePath: 'public/images/'
 };
+
+module.exports = imageConfig;
