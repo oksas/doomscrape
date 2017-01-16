5739260ce249e478f81a3d6b47da0f795cd703cf
John Oksasoglu <john.oksasoglu@gmail.com>
2017-01-16 15:31:24 -0800

    Clean up getImages

--- worker/getImages.js
+++ worker/getImages.js
@@ -10,21 +10,19 @@ function getImages(url) {
 		let hasPosts = pageUtils.hasPosts(document);
 
 		if (!hasPosts) {
-			// return callback(`No posts found on this page.`);
+			// handle with logger
 		}
 
 		let count = pageUtils.getPostCount(document);
 
 		if (count < 30) {
-			// return callback(`page ${url}\n has less than 30 posts on it; ABORT`);
+			// handle with logger
 		}
 
 		// images can contain items that are either just image data objects, or
 		// promises that resolve to image data objects fetched on Imgur or something
 		let images = [];
 
-		// will need to use Promise.all here to account for getting imgur images,
-		// which is another async operation
 		for (var i = 0; i < count; i++) {
 			let postData = pageUtils.getPostData(document, i);
 			if (postData) {
